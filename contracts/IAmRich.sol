f68c7e2027f3b1e0d5a7af7e531cdef40d33909f
Jimmy Hu <jim2212001@gmail.com>
2018-10-06 15:25:00 +0800

    Change indent to 2. Add constructor to IAmRich contract

--- contracts/IAmRich.sol
+++ contracts/IAmRich.sol
@@ -3,21 +3,25 @@ pragma solidity ^0.4.24;
 import "openzeppelin-solidity/contracts/ownership/Ownable.sol";
 
 contract IAmRich is Ownable {
-    struct Person {
-        address addr;
-        string name;
-        string msg;
-        uint amount;
-    }
+  struct Person {
+    address addr;
+    string name;
+    string msg;
+    uint amount;
+  }
 
-    Person public richPerson;
+  Person public richPerson;
 
-    function proofOfRich(string _name, string _msg) external payable  {
-        require(msg.value > richPerson.amount, "You are not rich enough.");
-        richPerson = Person(msg.sender, _name, _msg, msg.value);
-    }
+  constructor(string _name, string _msg) public {
+    richPerson = Person(msg.sender, _name, _msg, 1 ether);
+  }
 
-    function claim() external onlyOwner  {
-        owner.transfer(address(this).balance);
-    }
+  function proofOfRich(string _name, string _msg) external payable {
+    require(msg.value > richPerson.amount, "You are not rich enough.");
+    richPerson = Person(msg.sender, _name, _msg, msg.value);
+  }
+
+  function claim() external onlyOwner {
+    owner.transfer(address(this).balance);
+  }
 }
--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,5 @@
 var IAmRich = artifacts.require("./IAmRich.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(IAmRich);
+  deployer.deploy(IAmRich, "Trump", "I am rich.");
 };
